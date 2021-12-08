b5c5f9cefbdd854fadfb3627f775ac788ce8d434
karima-dev <karimakhadir1@gmail.com>
2021-12-08 21:00:57 +0100

    make it better

--- Configs/firebase.js
+++ Configs/firebase.js
@@ -3,13 +3,13 @@ require("firebase/database");
 
 let variables = require("./variables");
 let config = {
-  apiKey: variables.firebaseConfig.apiKey,
-  authDomain: variables.firebaseConfig.authDomain,
-  projectId: variables.firebaseConfig.projectId,
-  storageBucket: variables.firebaseConfig.storageBucket,
-  messagingSenderId: variables.firebaseConfig.messagingSenderId,
-  appId: variables.firebaseConfig.appId,
-  databaseURL: variables.firebaseConfig.databaseURL,
+  apiKey: "AIzaSyDYtQ1YLUUqqE_L-vBoEBF2wWzfxN6lXiU",
+  authDomain: "mabiblio-7a91f.firebaseapp.com",
+  projectId: "mabiblio-7a91f",
+  storageBucket: "mabiblio-7a91f.appspot.com",
+  messagingSenderId: "191802065243",
+  appId: "1:191802065243:web:f8f97d7ee4dc1f61e99a7e",
+  databaseURL: "https://mabiblio-7a91f-default-rtdb.firebaseio.com",
 };
 
 firebase.initializeApp(config);
